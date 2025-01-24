e3a8ad9d4b2ac7a9dd87287852281542a7d34487
wonmaster <mahfoud.merzoug@protonmail.com>
2025-01-24 19:07:08 +0100

    movement to tetromino and styling
    Adding gravity effect and moving left/right and speeding down.
    adding border to the blocks

--- scirpt.js
+++ scirpt.js
@@ -7,15 +7,17 @@ class Tetrominos {
     }
     draw(x, y,brick,ctx) {
         ctx.fillStyle = brick.color;
-        
+        ctx.strockStyle = 'black';
+        ctx.lineWidth = 2 ;
         for (let i = 0; i < this.shape.length; i++) {
             for (let j = 0; j < this.shape[i].length; j++) {
                 if(this.shape[i][j] === 1) {
                     ctx.fillRect(x + j * brick.length, y + i * brick.width, brick.length, brick.width);
+                    ctx.strokeRect(x + j * brick.length, y + i * brick.width, brick.length, brick.width);
                 }
             }
         }
-        ctx.strockStyle = 'white';
+        
     }
 }
 
@@ -95,8 +97,6 @@ function init() {
     canvas.width = 500;
     canvas.height = 680;
     let ctx = canvas.getContext('2d');
-    ctx.fillStyle = 'red';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
     console.log('Tetris');
     let otetro=new TTetro();
     let brick = new Brick();
@@ -107,6 +107,41 @@ function init() {
     // ctx.fillRect(0, 0, brick.length, brick.width);
     // ctx.strockStyle = 'black';
     // console.log('Brick');
+    document.addEventListener('keydown', (event) => {
+        if (event.key === 'ArrowLeft') {
+            // Check if moving left stays within canvas bounds
+            if (otetro.pos_x > 0) {
+                otetro.pos_x -= 30; // Move left by 30 pixels
+            }
+        } else if (event.key === 'ArrowRight') {
+            // Check if moving right stays within canvas bounds
+            if (otetro.pos_x + otetro.shape[0].length * brick.length < canvas.width){
+                otetro.pos_x += 30; 
+            }
+        } else if (event.key === 'ArrowDown') {
+            // Check if moving down stays within canvas bounds
+            if (otetro.pos_y + otetro.shape.length * brick.width < canvas.height) {
+                otetro.pos_y += 5; 
+            }
+        }
+    });
+
+    function gameloop() {
+        // Clear the canvas
+        ctx.fillStyle = 'lightgrey';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        // Update Tetromino position
+        if (otetro.pos_y + otetro.shape.length * brick.width < canvas.height) {
+            otetro.pos_y += 5; // Move down
+        }
+
+        
+        otetro.draw(otetro.pos_x, otetro.pos_y, brick, ctx);
+    }
+
+    // Start the game loop, calling it every 100 Millisecond
+    setInterval(gameloop, 100);
 }
 
 (init)();
